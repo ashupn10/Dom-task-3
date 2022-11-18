1c9ce06074a4f68252728c222ae2c7abaed0518f
ashupn10 <68610225+ashupn10@users.noreply.github.com>
2022-11-18 10:04:18 +0530

    Add files via upload

---
+++ DomTask3.js
@@ -0,0 +1,9 @@
+const header=document.getElementById('header-title');
+header.textContent="Hello";
+header.style.borderBottom= 'solid 3px #000';
+console.log('hello world');
+const itemhead= document.querySelector('.title');
+itemhead.style.fontWeight='bold';
+itemhead.style.color='Green';
+const title = document.getElementsByTagName('title');
+title.style.color='green';
